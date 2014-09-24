fdf336dae78836bf22ab867b49b0097914706370
behran@katu.com.tr <behran@katu.com.tr>
2014-09-24 11:50:19 +0300

    First Commit. Files added

---
+++ AjaxMvcUploader/AjaxMvcUploader/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxMvcUploader.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /File/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult UploadFile(HttpPostedFileBase file)
+        {
+            var result = DoSomethingWithFile(file);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        private bool DoSomethingWithFile(HttpPostedFileBase file)
+        {
+            try
+            {
+                var strFileLocation = "/temp/" + file.FileName;
+
+                file.SaveAs(HttpContext.Server.MapPath(strFileLocation));
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+	}
+}
---
+++ AjaxMvcUploader/AjaxMvcUploader/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AjaxMvcUploader.MvcApplication" Language="C#" %>
